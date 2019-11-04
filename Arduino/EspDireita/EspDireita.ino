d4e25a6a0bf5dcfc1457179259e774c15be08278
RODRIGO LORENZZON BASSO <17103308@pucrs.br>
2019-11-04 19:39:06 -0200

    Esps

---
+++ Arduino/EspDireita/EspDireita.ino
@@ -0,0 +1,12 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial s(14,12,false);
+
+void setup() {
+  s.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- Arduino/EspEsquerda/EspEsquerda.ino
+++ Arduino/EspEsquerda/EspEsquerda.ino
@@ -1,20 +1,20 @@
 #include <LiquidCrystal.h>
 #include <DHTesp.h>
-
+#include <Servo.h>
 #include <ESP8266WiFi.h>
 
-// pino 10 reservado para motor
-
-// functions headers
 void print_dht(float,float);
-
-// LCD setup
+void gira_motor();
+void gira_motor2();
+Servo servo;
+int servoPin = 4;
+int pos;
               //rs, enable, d4, d5, d6, d7
 LiquidCrystal lcd(0, 2, 14, 12, 13, 15);
-
-// dht 11 setup
 DHTesp dht;
 int dhtPin = 16;
+
+char estado = 0; // 0 fechado 1 aberto
 
 // wifi setup
 const char* ssid = "P30_IOT";
@@ -27,9 +27,13 @@ void setup()
   Serial.begin(115200);
   dht.setup(dhtPin, DHTesp::DHT11); // Connect DHT sensor to GPIO 16
 
+  Serial.println(WiFi.localIP());
+
   WiFi.begin(ssid, password);
   server.begin();
   lcd.begin(16, 2);
+
+  servo.attach(servoPin);
 }
 
 void loop()
@@ -42,7 +46,7 @@ void loop()
   print_dht(humidity, temperature);
 
   char str[30];
-  sprintf(str,"T %02.1f H %02.1f\n;",temperature,humidity);
+  sprintf(str,"%02.1f %02.1f;",temperature,humidity);
 
   char temp[30];
   sprintf(temp,"Temperatura %2.1fC",temperature);
@@ -51,10 +55,8 @@ void loop()
   sprintf(humi,"Umidade %2.1f%%",humidity);
 
   lcd.clear();
-  
   lcd.setCursor(0,0);
   lcd.print(temp);
-
   lcd.setCursor(0,1);
   lcd.print(humi);
 
@@ -76,27 +78,46 @@ void loop()
     while (client.connected()) {
       if (client.available()) {
         
-       String line = client.readStringUntil('.');
+       String line = client.readStringUntil(';');
        Serial.println(line);
+
+       if(strcmp(line,"Abrir") == 0)
+       {
+         if(estado == 0)
+         {
+           gira_motor();
+           estado = 1;
+         }
+          
+       }
+       else if(strcmp(line,"Fechar") == 0)
+       {
+         if(estado == 1)
+         {
+           gira_motor2();
+           estado = 0; 
+         }
+       }
       }
     }
     Serial.println("Client disconnected.");
     client.stop();
   }
   ////////////////// Wifi //////////////////////
-
-  
-  delay(2000);
 }
 
-void print_dht(float humidity, float temperature)
+void gira_motor()
 {
-  Serial.print(dht.getStatusString());
-  Serial.print("\n");
-  Serial.print("Umidade: ");
-  Serial.print(humidity, 1);
-  Serial.print("%\nTemperatura: ");
-  Serial.print(temperature, 1);
-  Serial.print("\n");
-  Serial.println(WiFi.localIP());
+  for(pos = 0; pos < 180; pos++){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
+    servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
+    delay(3); //INTERVALO DE 15 MILISSEGUNDOS
+  }    
+}
+
+void gira_motor2()
+{
+  for(pos = 180; pos < 0; pos--){ //PARA "pos" IGUAL A 0, ENQUANTO "pos" MENOR QUE 180, INCREMENTA "pos"
+    servo.write(pos); //ESCREVE O VALOR DA POSIÇÃO QUE O SERVO DEVE GIRAR
+    delay(3); //INTERVALO DE 15 MILISSEGUNDOS
+  }    
 }
